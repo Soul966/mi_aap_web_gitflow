74e299bafbfb6284ef02021c3fb32e9e5bd2b677
Soul <soulofyokai08@gmail.com>
2024-10-21 13:51:50 -0500

    primera fusion con release

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('btnClick').addEventListener('click', function() {
     document.getElementById('output').textContent = '¡Botón clicado!';
 });
 
-
+//este es un metodo, suscribete a la tecnologia avanzada
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var name = document.getElementById('name').value;
